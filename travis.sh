96abdb92a6e6d4a846c9e2384802daa4c35fb00c
heartsucker <heartsucker@autistici.org>
2017-06-10 14:45:41 +0200

    fix travis build

--- travis.sh
+++ travis.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -ex
 
-pip install virtualenv
-
+sudo pip install virtualenv
 cd tests/tuf-test-vectors
 make init
 cd ../../
@@ -11,9 +10,4 @@ trap '{ rc=$?; cat Cargo.lock; exit $rc; }' EXIT
 RUST_BACKTRACE=full cargo build --verbose --features=cli
 
 ./tests/tuf-test-vectors/server.py --path tuf &>/dev/null &
-trap '{ rc=$?; kill %1; cat Cargo.lock; exit $rc; }' EXIT
 RUST_BACKTRACE=full cargo test --verbose --features=cli
-kill %1 || true
-
-trap - EXIT
-trap
