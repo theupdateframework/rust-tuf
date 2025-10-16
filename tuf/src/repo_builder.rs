18ce518fe765cbc242796e8044a6311d9cd8d56c
Thomas Zander <thomasalpinus@google.com>
2025-10-16 18:40:32 +0200

    [tuf][interop-tests] Migrate lazy_static to LazyLock
    * Use LazyLock (stable after 1.80.0)
    * Require 1.80.0 as the minimum Rust version
    * While on it: Update most dependencies
    * Bump version to 0.3.0-beta14

--- tuf/src/repo_builder.rs
+++ tuf/src/repo_builder.rs
@@ -1582,27 +1582,25 @@ mod tests {
         },
         futures_executor::block_on,
         futures_util::io::{AsyncReadExt, Cursor},
-        lazy_static::lazy_static,
         maplit::hashmap,
         pretty_assertions::assert_eq,
         std::collections::BTreeMap,
+        std::sync::LazyLock,
     };
 
-    lazy_static! {
-        static ref KEYS: Vec<Ed25519PrivateKey> = {
-            let keys: &[&[u8]] = &[
-                include_bytes!("../tests/ed25519/ed25519-1.pk8.der"),
-                include_bytes!("../tests/ed25519/ed25519-2.pk8.der"),
-                include_bytes!("../tests/ed25519/ed25519-3.pk8.der"),
-                include_bytes!("../tests/ed25519/ed25519-4.pk8.der"),
-                include_bytes!("../tests/ed25519/ed25519-5.pk8.der"),
-                include_bytes!("../tests/ed25519/ed25519-6.pk8.der"),
-            ];
-            keys.iter()
-                .map(|b| Ed25519PrivateKey::from_pkcs8(b).unwrap())
-                .collect()
-        };
-    }
+    static KEYS: LazyLock<Vec<Ed25519PrivateKey>> = LazyLock::new(|| {
+        let keys: &[&[u8]] = &[
+            include_bytes!("../tests/ed25519/ed25519-1.pk8.der"),
+            include_bytes!("../tests/ed25519/ed25519-2.pk8.der"),
+            include_bytes!("../tests/ed25519/ed25519-3.pk8.der"),
+            include_bytes!("../tests/ed25519/ed25519-4.pk8.der"),
+            include_bytes!("../tests/ed25519/ed25519-5.pk8.der"),
+            include_bytes!("../tests/ed25519/ed25519-6.pk8.der"),
+        ];
+        keys.iter()
+            .map(|b| Ed25519PrivateKey::from_pkcs8(b).unwrap())
+            .collect()
+    });
 
     fn create_root(
         version: u32,
